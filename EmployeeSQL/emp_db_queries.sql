44f7a7432f7d52c32fcf4d98adc2bb898ed223db
RanjaniAnjurVenkatraman <48039364+RanjaniAnjurVenkatraman@users.noreply.github.com>
2021-08-17 00:10:12 -0400

    Update emp_db_queries.sql

--- EmployeeSQL/emp_db_queries.sql
+++ EmployeeSQL/emp_db_queries.sql
@@ -21,3 +21,44 @@ left join employees e
 on m.emp_no = e.emp_no;
 
 --List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+left join dept_emp de
+on e.emp_no = de.emp_no
+left join departments d
+on de.dept_no = d.dept_no
+
+-- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+select first_name,last_name, sex
+from employees
+where first_name = 'Hercules'
+and last_name like 'B%';
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+left join dept_emp de
+on e.emp_no = de.emp_no
+left join departments d
+on de.dept_no = d.dept_no
+where d.dept_name = 'Sales';
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from employees e
+left join dept_emp de
+on e.emp_no = de.emp_no
+left join departments d
+on de.dept_no = d.dept_no
+where d.dept_name in ('Sales','Development') ;
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(*) AS frequency_count
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_count DESC;
